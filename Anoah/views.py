f77b67cfac9bcf03dc475cf5d8f5972961d8bbf5
Nick Chen <cjyx9@outlook.com>
2020-08-24 21:25:51 +0800

    ✨ 我也不知道说啥~

--- Anoah/views.py
+++ Anoah/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 from .FakeAnoah import User as AnoahUser
-anoah_user = AnoahUser('1765840')
+from users.models import UserSelf
+from django.contrib.auth.decorators import login_required
+
 def index(request):
-    result = {
-        'undo_homework_num': anoah_user.get_undo_homework(1)['homework_count'],
-        'name': anoah_user.user_name,
-    }
+    if request.user.is_authenticated:
+        anoah_user = AnoahUser(UserSelf.objects.get(userName=request.user.username).anoahName.replace('e',''))
+        result = {
+            'undo_homework_num': anoah_user.get_undo_homework(1)['homework_count'],
+            'name': anoah_user.user_name
+        }
+    else:
+        result = {}
     return render(request,'index.html', result)
 
+@login_required
 def homework(request):
+    anoah_user = AnoahUser(UserSelf.objects.get(userName=request.user.username).anoahName.replace('e',''))
     info = request.GET
     try:
         page = info['page']
     except:
         page = 0
-    print(page, type(page))
     result = {
         'undo_homework': anoah_user.get_undo_homework(page),
         'pages_count': str(anoah_user.get_undo_homework_page()-1),
--- junbo_online/settings.py
+++ junbo_online/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+LOGIN_URL = '/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
